2b1707b49fc79151237ed442df539cba8ba79b85
Mauya <smkadungure@live.co.uk>
2017-11-26 23:52:30 +0000

    Initial commit

---
+++ settings/dev.py
@@ -0,0 +1,14 @@
+from base import *
+
+DEBUG = True
+
+INSTALLED_APPS.append('debug_toolbar')
+
+MIDDLEWARE.append('debug_toolbar.middleware.DebugToolbarMiddleware')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
