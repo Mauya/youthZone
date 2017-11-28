6a40098777cbc20a2694a45c9f5d50ffd958e8fa
Mauya <smkadungure@live.co.uk>
2017-11-28 22:36:06 +0000

    Settings, static files and templates

---
+++ youthZone/test.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+from django.core.urlresolvers import reverse
+
+class TestHomePage(TestCase):
+    def test_uses_home_template(self):
+        response = self.client.get(reverse('home'))
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_uses_base_template(self):
+        response = self.client.get(reverse('home'))
+        self.assertTemplateUsed(response, 'base.html')
--- youthZone/urls.py
+++ youthZone/urls.py
@@ -17,12 +17,15 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
+from youthZone import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.home, name='home'),
 ]
 
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns.append(url(r'^debug/', include(debug_toolbar.urls)))
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(r'/favicon.ico', document_root='static/favicon.ico')
