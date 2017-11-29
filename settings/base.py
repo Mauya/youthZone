6fb2bf47fed43cdcbfa2ab1da91023a2053cddc4
Mauya <smkadungure@live.co.uk>
2017-11-29 23:34:16 +0000

    modified ALLOWED_HOSTS

--- settings/base.py
+++ settings/base.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'SECRET_KEY'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 SITE_ID = 2
 INTERNAL_IPS = '127.0.0.1'
 
