6e10eea4d778ba890b2958f0fcf67334b45d4326
Mauya <smkadungure@live.co.uk>
2017-12-02 09:55:32 +0000

    added reset pssword views

--- account/urls.py
+++ account/urls.py
@@ -4,7 +4,11 @@ from django.contrib.auth.views import (
     logout,
     logout_then_login,
     password_change,
-    password_change_done
+    password_change_done,
+    password_reset,
+    password_reset_done,
+    password_reset_confirm,
+    password_reset_complete
 )
 from account import views
 
@@ -21,4 +25,10 @@ urlpatterns = [
     url(r'^password-change/$', password_change, name='password_change'),
     url(r'^password-change/done/$', password_change_done, name='password_change_done'),
 
+    # restore password urls
+    url(r'^password-reset/$', password_reset, name='password_reset'),
+    url(r'^password-reset/done/$', password_reset_done, name='password_reset_done'),
+    url(r'^password-reset/confirm/(?P<uidb64>[-\w]+)/(?P<token>[-\w]+)/$', password_reset_confirm,
+        name='password_reset_confirm'),
+    url(r'^password-reset/complete/$', password_reset_complete, name='password_reset_complete'),
 ]
